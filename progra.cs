d9cd8450e1210a4208748dbbb463136008ff6a10
to0ken <evgeniamafinova@gmail.com>
2026-04-28 09:51:55 +0300

    Update progra.cs

--- progra.cs
+++ progra.cs
@@ -1,36 +1,57 @@
 using System;
+using System.Numerics;
 
-namespace Consolec
+public class Program
 {
-    internal class Program
+    public static string LongestCommonPrefix(string[] strs)
     {
-        static void Main(string[] args)
-        {
+        // количество строк 
+        int len = strs.Length;
+        if (len == 0) return "";
 
-            string srt = "KOS123!";
-            string symbol = "!";
-            // ToUpper - текст большой
-            // replise - замена значений
-            if (int.TryParse(srt, out int result))
+        string prefix = strs[0];
+        for (int i = 1; i < len; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
             {
-                Console.WriteLine("+ point {reult}");
-            }
-            else
-            {
-                Console.WriteLine("no number");
-            }
-            if (srt.Contains(symbol))
-            {
-                Console.WriteLine("+ point good there is a symbol");
-            }
-            else
-            {
-                Console.WriteLine("no symbol");
-            }
-                
+                // убираем последний симыол из префикса
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "") return "";
             }
         }
+
+        return prefix;
     }
- 
 
 
+    static BigInteger FactNaive(int n)
+    {
+        BigInteger r = 1;
+        for (int i = 2; i <= n; ++i)
+            r *= i;
+        return r;
+    }
+
+    // ghbybvftn lfyyst b sjpshfoftn
+    // -----------------------------------------------------------------
+    // Тип BigInteger — это неизменяемый тип,
+    // представляющий произвольно большое целое число,
+    // значение которого в теории не имеет верхних или нижних границ. 
+    // -----------------------------------------------------------------
+
+
+public static void Main()
+    {
+        Console.WriteLine("conclusion");
+
+        Console.WriteLine(string.Format("Otvet = {0}",
+            LongestCommonPrefix(new string[] { "momis", "momus", "momps" }))); // "mom"
+
+        Console.WriteLine(string.Format("Otvet = {0}",
+            LongestCommonPrefix(new string[] { "six", "stop", "true" }))); // ""
+
+        Console.WriteLine(FactNaive(11));
+    }
+   
+
+}
