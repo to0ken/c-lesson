888d141ac6a3c2eb3a03256d027a1fbc35044a95
to0ken <evgeniamafinova@gmail.com>
2026-05-05 09:43:50 +0300

    Update progra.cs

--- progra.cs
+++ progra.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Numerics;
 
-public class Program
+
+ class Program
 {
-    public static string LongestCommonPrefix(string[] strs)
+    static string LongestCommonPrefix(string[] strs)
     {
         // количество строк 
         int len = strs.Length;
@@ -40,9 +41,48 @@ public class Program
     // -----------------------------------------------------------------
 
 
-public static void Main()
+
+    // ЗАДАНИЕ 1 ------------ вывод только четных значний в массиве через функцию -----------------
+    // void - не требуется return
+
+    static void EvenNumbers(int[] n)
     {
-        Console.WriteLine("conclusion");
+        //int count = 0;
+        int[] arr = { 3, 5, 6, 7, 12, 22, 24, 25 };
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                Console.WriteLine(arr[i]);
+                //count++;
+
+            }
+        }
+        //return count;
+    }
+
+    // --------------------------- СОРТИРОВКА МАССИВА ПО КОЛИЧЕСТВУ ДЕЛИТОЕЙ
+
+    static int numbertest(int n)
+    {
+        if (n == 0) return 0;
+        int count = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+    static void Main()
+    {
+        /*Console.WriteLine("conclusion");
 
         Console.WriteLine(string.Format("Otvet = {0}",
             LongestCommonPrefix(new string[] { "momis", "momus", "momps" }))); // "mom"
@@ -50,8 +90,16 @@ public static void Main()
         Console.WriteLine(string.Format("Otvet = {0}",
             LongestCommonPrefix(new string[] { "six", "stop", "true" }))); // ""
 
-        Console.WriteLine(FactNaive(11));
-    }
-   
+        Console.WriteLine(FactNaive(11));*/
 
+        //  ЗАДАНИЕ 1 ===========================================================
+        int[] marr = { 3, 5, 6, 7, 12, 22, 24, 25 };
+        EvenNumbers(marr);
+        Console.WriteLine(marr);
+
+        int[] num = { 4, 8, 16 };
+
+
+        
+    }
 }
