e700a6eb4eb8a85e01fdd5ff014ae58ff1aaaf79
to0ken <evgeniamafinova@gmail.com>
2026-05-26 10:35:17 +0300

    Create proga6.cs

---
+++ dz/proga6.cs
@@ -0,0 +1,58 @@
+
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+
+class Money
+{
+    
+    public void ShowRate(string from, string to, decimal rate)
+    {
+        Console.WriteLine($"1 {from} = {rate} {to}");
+    }
+}
+
+
+class USD : Money
+{
+    public void GetInfo()
+    {
+        
+        base.ShowRate("USD", "RUB", 92.50m);
+        base.ShowRate("USD", "EUR", 0.92m);
+    }
+}
+
+class RUB : Money
+{
+    public void GetInfo() => base.ShowRate("RUB", "USD", 0.011m);
+}
+
+class EUR : Money
+{
+    public void GetInfo() => base.ShowRate("EUR", "USD", 1.08m);
+}
+
+class UAN : Money
+{
+    public void GetInfo() => base.ShowRate("UAN", "RUB", 12.80m);
+}
+
+class Peso : Money
+{
+    public void GetInfo() => base.ShowRate("Peso", "USD", 0.058m);
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+
+        new USD().GetInfo();
+        new RUB().GetInfo();
+        new EUR().GetInfo();
+        new UAN().GetInfo();
+        new Peso().GetInfo();
+    }
+}
