a4671629e640f191dc472de07a765251f3cfbfd4
to0ken <evgeniamafinova@gmail.com>
2026-05-27 09:24:45 +0300

    Create proga12.cs

---
+++ proga12.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+
+//4.Отсортировать массив строк по алфавиту с помощью LINQ
+class Program
+{
+static void Main()
+    {
+        string[] words = { "яблоко", "арбуз", "банан", "кот", "дом" };
+
+ 
+        string[] sorted = words.OrderBy(w => w).ToArray();
+
+        Console.WriteLine(string.Join(", ", sorted));
+    }
+}
